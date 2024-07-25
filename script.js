b0be583a2ca7f75c1ff8ea7b877dddf02da4e636
devkrishna8 <161730838+rubypoddar@users.noreply.github.com>
2024-07-25 20:44:56 +0530

    calculator using jquery
    Dynamic ui Calculator

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-$(function(){
-
-    // when a value is clicked
-    $(".val").click(function(e){
+$(function() {
+  // when a value is clicked
+  $(".val").click(function(e) {
       // prevent the link from acting like a link
       e.preventDefault();
-      //grab this link's href value
-        var a = $(this).attr("href");
+      // grab this link's href value
+      var a = $(this).attr("href");
       // append said value to the screen
-          $(".screen").append(a);
+      $(".screen").append(a);
       // append same value to a hidden input
-          $(".outcome").val($(".outcome").val() + a);
-          
-      });
-    // when equals is clicked
-      $(".equal").click(function(){
-      // solve equation and put in hidden field
-          $(".outcome").val(eval($(".outcome").val()));
-      // take hidden field's value & put it on screen
-          $(".screen").html(eval($(".outcome").val()));
-      });
-    // clear field
-      $(".clear").click(function(){
+      $(".outcome").val($(".outcome").val() + a);
+  });
+
+  // when equals is clicked
+  $(".equal").click(function() {
+      try {
+          // solve equation and put in hidden field
+          let result = eval($(".outcome").val());
+          $(".outcome").val(result);
+          // take hidden field's value & put it on screen
+          $(".screen").html(result);
+      } catch (e) {
+          $(".screen").html("Error");
           $(".outcome").val("");
-          $(".screen").html("");
-      });
-    // minimize (looks kinda cool?)
-      $(".min").click(function(){
-          $(".cal").stop().animate({
-              width: "0px", height: "0px", marginLeft: "700px", marginTop: "1000px"
-          }, 500);
-          setTimeout(function(){$(".cal").css("display", "none")}, 600);
-      });
-    //close window. refresh to get back
-      $(".close").click(function(){
-          $(".cal").css("display", "none");
-      })
-  })
+      }
+  });
+
+  // clear field
+  $(".clear").click(function() {
+      $(".outcome").val("");
+      $(".screen").html("");
+  });
+
+  // minimize (looks kinda cool?)
+  $(".min").click(function() {
+      $(".cal").stop().animate({
+          width: "0px", height: "0px", marginLeft: "700px", marginTop: "1000px"
+      }, 500);
+      setTimeout(function() {
+          $(".cal").css("display", "none")
+      }, 600);
+  });
+
+  // close window. refresh to get back
+  $(".close").click(function() {
+      $(".cal").css("display", "none");
+  });
+});
